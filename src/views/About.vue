fd7a266abd656ccbc4aa6b90a4fc7c1fe36f01d8
enzodmrib <enzorib2@gmail.com>
2021-11-04 22:46:27 -0300

    Adds few text changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>I've created this project to send it as an activity for my web development class</p>
   </div>
 </template>
